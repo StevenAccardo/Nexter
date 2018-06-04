1c9e817e48ddd77a8b34ae58da8ab708bc466b3b
Steven Accardo <accardo.steven@gmail.com>
2018-06-04 15:34:36 -0700

    First commit. Project is complete and live

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+//Webpack 4
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'src/js/[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/fonts/',
+            publicPath: 'assets/fonts/'
+          }
+        }
+      },
+      {
+        test: /\.pdf$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/docs/',
+            publicPath: 'assets/docs/'
+          }
+        }
+      },
+      {
+        test: /\.(mp3|wav|wma|ogg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/audio/',
+            publicPath: 'assets/audio/'
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/images/',
+              publicPath: 'assets/images/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/videos/',
+            publicPath: 'assets/videos/'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new FaviconsWebpackPlugin('./assets/images/favicon.png'),
+    new CleanWebpackPlugin('dist', {}),
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css'
+    })
+  ]
+};
